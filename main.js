2ebc8b5a1dab1e3e0c0651185564d5e834ff28b7
Severinkirill <severinkirill09@gmail.com>
2025-06-10 21:06:49 +0300

    1 edit of HW

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(typeof "text");
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol('id'));
+console.log(typeof 1n); // BigInt
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function(){});
